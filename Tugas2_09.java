a67105f19a678159f7e39ccacb1abfba27b9a2a3
ddickyy <dikiidarmawam05x2@gmail.com>
2024-09-11 14:36:39 +0700

    Perbaikan 2

--- Tugas2_09.java
+++ Tugas2_09.java
@@ -4,15 +4,15 @@ public class Tugas2_09 {
     public static void main(String[] args) {
     
         Scanner gaji = new Scanner (System.in);
-        double jmlJam, upah, gajiAkhir, gajiAwal, bonus, pajak;
+        long jmlJam, upah, gajiAkhir, gajiAwal, bonus, pajak;
 
         System.out.println("Masukkan jumlah jam kerja Anda: ");    
-        jmlJam = gaji.nextDouble();
+        jmlJam = gaji.nextLong();
         System.out.println("Masukkan upah per jam Anda: ");
-        upah = gaji.nextDouble();
+        upah = gaji.nextLong();
             
         gajiAwal = upah*jmlJam;
-        gajiAkhir = (gajiAwal + (gajiAwal*10/100)) - (gajiAwal*5/100);
+        gajiAkhir = ((gajiAwal + (gajiAwal*10/100)) - (gajiAwal*5/100)) *30;
         
         System.out.println("Gaji akhir Anda adalah Rp" +gajiAkhir);
     }
